8dd3562fb3dd76de8c8e348a910d556d2ad30666
AlehandroSV <alehandrosantos99@gmail.com>
2023-07-24 15:29:28 -0300

    feat: Criando controller no Laravel

---
+++ PHP/B7/laravel/app/Http/Controllers/SiteController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SiteController extends Controller
+{
+    //
+    public function index()
+    {
+        // Aqui eu poderia criar uma lógica...
+        // Verificar se um usuário existe
+        // Buscar dados de um usuário, etc...
+
+        return view('bemvindo');
+    }
+}
